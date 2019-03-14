a6d5e1e51e25b383ca27c6f1e2fe3550b51e8aa1
Mark Gilgallon <mark.gilgallon@f1000.com>
2019-03-14 16:58:27 +0000

    Initial Commit - The Minimal / React / Webpack / Babel / Sass / Styled Components Setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        loader: 'sass-extract-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist'
+  },
+  watch: true
+};
