ac0ec181305cf0d4f73d94806fcf3fb43e7f7032
Mark Gilgallon <mark.gilgallon@f1000.com>
2019-03-26 13:19:59 +0000

    tidy up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
-const Wrapper = () => {return <p>Minimal React / Webpack / Babel Boilerplate</p>}
+const Wrapper = () => {return <p>Material UI - Basic</p>}
 
 ReactDOM.render(
   
     <Wrapper>
-
+      <CssBaseline />
     </Wrapper>
   
   ,document.getElementById('app')
