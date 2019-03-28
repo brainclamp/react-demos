a3cd1ca4b91c0a95bff1d81f157b3d0b3389226a
Mark Gilgallon <mark.gilgallon@f1000.com>
2019-03-28 15:59:47 +0000

    adapting styled component API

--- src/index.js
+++ src/index.js
@@ -33,6 +33,24 @@ const StyledComponentButton = styled(Button)({
   height: 48,
   padding: '0 30px'
 });
+// adapting
+const styledBy = (property, mapping) => props => mapping[props[property]];
+
+const AdaptedStyledComponentButton = styled(({ color, ...other }) => <Button {...other} />)({
+  background: styledBy('color', {
+    red: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    blue: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+  }),
+  border: 0,
+  borderRadius: 10,
+  boxShadow: styledBy('color', {
+    red: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    blue: '0 3px 5px 2px rgba(33, 203, 243, .3)',
+  }),
+  color: 'white',
+  height: 56,
+  padding: '0 30px',
+});
 
 // Higher-order component API
 const styles = {
@@ -46,7 +64,7 @@ const styles = {
     padding: '0 30px'
   },
 };
-function HigherOrderComponent(props) {
+const HigherOrderComponent = (props) => {
   const { classes } = props;
   return <Button className={classes.root}>Higher-order component</Button>;
 }
@@ -63,9 +81,11 @@ ReactDOM.render(
         Hello World
       </Button>
 
-      <Hook/>
+      <Hook background="#FFF" />
 
       <StyledComponentButton>Styled Components API</StyledComponentButton>
+      <AdaptedStyledComponentButton color="red">Red</AdaptedStyledComponentButton>
+      <AdaptedStyledComponentButton color="blue">Blue</AdaptedStyledComponentButton>
 
       <HOCButton />
 
