67bcf930f9dd35bd5e294dbed2d18b0d0f39a2a0
Mark Gilgallon <mark.gilgallon@f1000.com>
2019-03-28 12:42:21 +0000

    tidy up

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import PropTypes from "prop-types";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
+import { palette, spacing, compose, typography } from '@material-ui/system';
+import { styled } from '@material-ui/styles';
+import { unstable_Box as Box } from '@material-ui/core/Box';
 
-const Wrapper = (props) => {return props.title}
-
-Wrapper.propTypes = {
-  title: PropTypes.string
-}
+const theme = createMuiTheme();
 
 ReactDOM.render(
-  <CssBaseline>
-    <Wrapper title='Material UI - Basic'>
-      <CssBaseline />
-    </Wrapper>
-  </CssBaseline>
+
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+    <p color="primary.main"
+    fontFamily="h6.fontFamily">text</p>
+    {/* <Box
+      color="primary.main"
+      bgcolor="background.paper"
+      fontFamily="h6.fontFamily"
+      fontSize={{ xs: 'h6.fontSize', sm: 'h4.fontSize', md: 'h3.fontSize' } }
+      p={{ xs: 2, sm: 3, md: 4} }
+    > 
+      @material-ui/system
+    </Box> */}
+   
+  </MuiThemeProvider>
+     
   ,document.getElementById('app')
 );
 
