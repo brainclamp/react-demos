75bb961e12384a58c2daa7cd3b53a356db9b5617
Mark Gilgallon <mark.gilgallon@f1000.com>
2019-03-29 15:18:02 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CssBaseline from '@material-ui/core/CssBaseline'
-
+import { createMuiTheme , styled } from '@material-ui/styles'
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const MyButton = styled(Button)({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  borderRadius: 20,
+  color: 'white',
+  height: '50px',
+});
+
 
 ReactDOM.render(
-  <CssBaseline>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-  </CssBaseline>
+  <React.Fragment>
+    <CssBaseline/>
+    
+    <MyButton>Hello world</MyButton>
+    
+    </React.Fragment>
+
   ,document.getElementById('app')
 );
 
 module.hot.accept();
+
